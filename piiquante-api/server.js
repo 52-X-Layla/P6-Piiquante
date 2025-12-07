ab43b9ef03e6e69d467d3e0d18d340145d6cdb5f
52-X-Layla <103341661+52-X-Layla@users.noreply.github.com>
2025-12-06 19:06:15 -0500

    Refactor MongoDB connection and middleware setup

--- piiquante-api/server.js
+++ piiquante-api/server.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-// importation des routes utilisateurs
 const userRoutes = require('./routes/user');
-
-// importation des routes sauces
 const sauceRoutes = require('./routes/sauce');
 
 // importation de helmet pour securite
@@ -14,23 +11,20 @@ const helmet = require('helmet');
 
 const path = require('path'); 
 
-// creation de l'application express
 const app = express();
 
-// ======== MONGODB CONNECTION ======== //
-const mongoUri = process.env.MONGO_URI || 'mongodb+srv://user1:pass12345@cluster0.vydnst8.mongodb.net/piiquante?retryWrites=true&w=majority&appName=Cluster0';
+// Connexion a MongoDB
+const mongoUri = process.env.MONGO_URI;
 mongoose.connect(
   mongoUri,
   { useNewUrlParser: true, useUnifiedTopology: true }
 )
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch((err) => console.log('Connexion à MongoDB échouée !', err));
-// ======================================= //
 
-// Middleware pour lire JSON dans les requetes
 app.use(express.json());
 
-// CORS Middleware (required for Angular frontend)
+// CORS Middleware 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -44,8 +38,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
-// Middleware pour logger les requetes entrantes
 app.use((req, res, next) => {
   console.log("📩 Incoming request:", req.method, req.url);
   next();
@@ -58,21 +50,11 @@ app.use(
   })
 );
 
-
-
-
-
-// Middleware pour servire les images statiques
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-
-// routes pour utilisateurs
 app.use('/api/auth', userRoutes);
 
-// routes pour sauces
 app.use('/api/sauces', sauceRoutes);
-
-
 
 app.get('/', (req, res) => {
   res.send('Server running');
