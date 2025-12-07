8b79c3923b577a00941943dc45849b9a80fa95b7
52-X-Layla <103341661+52-X-Layla@users.noreply.github.com>
2025-12-06 19:11:34 -0500

    Clean up user.js by removing commented code
    Removed commented-out code for user schema definition.

--- piiquante-api/models/user.js
+++ piiquante-api/models/user.js
@@ -1,17 +1,5 @@
-//const mongoose = require('mongoose');
-
-//const userSchema = mongoose.Schema({
-  //email: { type: String, required: true, unique: true },
-  //password: { type: String, required: true }
-//});
-
-//module.exports = mongoose.model('User', userSchema);
-
-
 const mongoose = require('mongoose');
 
-
-// Email regex for validation
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const userSchema = mongoose.Schema({
