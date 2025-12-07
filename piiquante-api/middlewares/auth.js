05a581ddad451c89cda55b1e18791e0b25f590d7
52-X-Layla <103341661+52-X-Layla@users.noreply.github.com>
2025-12-06 19:13:28 -0500

    Middleware

--- piiquante-api/middlewares/auth.js
+++ piiquante-api/middlewares/auth.js
@@ -1,4 +1,3 @@
-// importation du package jsonwebtoken pour gerer les tokens
 const jwt = require('jsonwebtoken');
 
 // Middleware d'authentification
@@ -6,20 +5,13 @@ module.exports = (req, res, next) => {
 
   try {
 
-    // Récupère le token envoyé par le frontend dans le header Authorization
-    // Format attendu : "Bearer TOKEN"
     const token = req.headers.authorization.split(' ')[1];
-
-    // Vérifie et décode le token avec la clé secrète
     const decodedToken = jwt.verify(token, 'RANDOM_SECRET_KEY');
-
-    // Récupère l'userId contenu dans le token
     const userId = decodedToken.userId;
-
-    // Ajoute l'userId au request pour l'utiliser dans les routes protégées
+    
     req.auth = { userId };
 
-    next(); // Passe au middleware ou contrôleur suivant
+    next(); 
 
   } catch (error) {
     res.status(401).json({ message: "Requête non authentifiée !" });
