22775644c93eebee1193ff5c8ecf5494200a7b68
52-X-Layla <103341661+52-X-Layla@users.noreply.github.com>
2025-12-06 19:20:56 -0500

    Update

--- piiquante-api/config/db.js
+++ piiquante-api/config/db.js
@@ -1,16 +1,18 @@
+// Import du module Mongoose pour communiquer avec MongoDB
 const mongoose = require('mongoose');
 
+// Fonction asynchrone qui établit la connexion à MongoDB
 async function connectDB() {
   const uri = process.env.MONGO_URI;
   console.log('DEBUG MONGO_URI =', uri);
   
   try {
     await mongoose.connect(uri);
-    console.log('✅ Connected to MongoDB');
+    console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
     process.exit(1);
   }
 }
-
+// Export de la fonction de conexion
 module.exports = connectDB;
